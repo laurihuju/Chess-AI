dcd661867980e4aa33f9577b219241a29e7fcc51
laurihuju <lauri.huju@gmail.com>
2025-01-20 12:50:42 +0200

    Create GameState destructor

--- Chess-AI/main/gameState.cpp
+++ Chess-AI/main/gameState.cpp
@@ -3,3 +3,15 @@
 GameState::GameState() {
 
 }
+
+GameState::~GameState() {
+	for (int i = 0; i < 8; i++) {
+		for (int j = 0; j < 8; j++) {
+			if (_board[i][j] == 0)
+				continue;
+
+			delete _board[i][j];
+		}
+	}
+
+}
