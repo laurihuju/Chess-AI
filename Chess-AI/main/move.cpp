77be80c3771adcc78fafd656b368ef606b854508
laurihuju <lauri.huju@gmail.com>
2025-01-21 14:20:49 +0200

    Created the move class

---
+++ Chess-AI/main/move.cpp
@@ -0,0 +1,48 @@
+#include "move.h"
+
+Move::Move(int x1, int y1, int x2, int y2) : _x1(x1), _y1(y1), _x2(x2), _y2(y2) {}
+
+Move::Move(const std::string& input) {
+	if (input.length() != 4) {
+		_x1 = 0;
+		_y1 = 0;
+
+		_x2 = 0;
+		_y2 = 0;
+
+		return;
+	}
+
+	_x1 = convertXCoordinateFromInput(input[0]);
+	_y1 = convertYCoordinateFromInput(input[1]);
+	_x2 = convertXCoordinateFromInput(input[2]);
+	_y2 = convertYCoordinateFromInput(input[3]);
+}
+
+int Move::x1() const {
+	return _x1;
+}
+int Move::y1() const {
+	return _y1;
+}
+
+int Move::x2() const {
+	return _x2;
+}
+int Move::y2() const {
+	return _y2;
+}
+
+int Move::convertXCoordinateFromInput(char coordinateInputChar) const {
+	if (coordinateInputChar < 'a' || coordinateInputChar > 'h')
+		return 0;
+
+	return (int)coordinateInputChar - 'a';
+}
+int Move::convertYCoordinateFromInput(char coordinateInputChar) const {
+	if (coordinateInputChar < '1' || coordinateInputChar > '8')
+		return 0;
+
+	return '8' - (int)coordinateInputChar;
+}
+
---
+++ Chess-AI/main/move.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <string>
+
+class Move {
+
+private:
+	int _x1;
+	int _y1;
+
+	int _x2;
+	int _y2;
+
+	int convertXCoordinateFromInput(char coordinateInputChar) const;
+	int convertYCoordinateFromInput(char coordinateInputChar) const;
+
+public:
+	Move(int x1, int y1, int x2, int y2);
+	Move(const std::string& input);
+
+	int x1() const;
+	int y1() const;
+
+	int x2() const;
+	int y2() const;
+
+};
