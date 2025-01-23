bd065f86eb784c8545661b3c710dea7fdb1b2540
laurihuju <lauri.huju@gmail.com>
2025-01-23 15:09:21 +0200

    Moves can now make promotion

--- Chess-AI/main/move.cpp
+++ Chess-AI/main/move.cpp
@@ -1,10 +1,26 @@
 #include <string>
 #include "move.h"
 
-Move::Move(int x1, int y1, int x2, int y2) : _x1(x1), _y1(y1), _x2(x2), _y2(y2) {}
+Move::Move(int x1, int y1, int x2, int y2) : _x1(x1), _y1(y1), _x2(x2), _y2(y2), _promotionPiece(-1) {}
+
+Move::Move(int x1, int y1, int x2, int y2, char promotionPiece) : _x1(x1), _y1(y1), _x2(x2), _y2(y2), _promotionPiece(promotionPiece) {}
 
 Move::Move(const std::string& input) {
-	if (input.length() != 4) {
+	// Parse promotion piece
+	if (input.length() == 5) {
+		if (input[4] != 'k' && input[4] != 'b' && input[4] != 'r' && input[4] != 'q') {
+			_promotionPiece = 'q';
+		}
+		else {
+			_promotionPiece = input[4];
+		}
+	}
+	else {
+		_promotionPiece = -1;
+	}
+
+	// Handle invalid length input
+	if (input.length() != 4 && input.length() != 5) {
 		_x1 = 0;
 		_y1 = 0;
 
@@ -14,10 +30,12 @@ Move::Move(const std::string& input) {
 		return;
 	}
 
+	// Handle valid length input
 	_x1 = convertXCoordinateFromInput(input[0]);
 	_y1 = convertYCoordinateFromInput(input[1]);
 	_x2 = convertXCoordinateFromInput(input[2]);
 	_y2 = convertYCoordinateFromInput(input[3]);
+
 }
 
 int Move::x1() const {
@@ -32,6 +50,10 @@ int Move::x2() const {
 }
 int Move::y2() const {
 	return _y2;
+}
+
+char Move::promotionPiece() const {
+	return _promotionPiece;
 }
 
 int Move::convertXCoordinateFromInput(char coordinateInputChar) const {
