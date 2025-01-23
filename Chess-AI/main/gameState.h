0627a7b8d5ef88393a9ff4b6abc237a17192c203
laurihuju <lauri.huju@gmail.com>
2025-01-23 10:57:59 +0200

    Added comments to some GameState functions

--- Chess-AI/main/gameState.h
+++ Chess-AI/main/gameState.h
@@ -17,12 +17,41 @@ private:
 	bool _lowerRightCastlingPossible = true; 
 
 public:
+	/// <summary>
+	/// Initializes the GameState with default board content.
+	/// </summary>
 	GameState();
+
 	~GameState();
 
+	/// <summary>
+	/// Moves the given move. Handles capture if the move moves a piece to a place where another piece is located.
+	/// Does not check if the move is valid!
+	/// </summary>
+	/// <param name="move"></param>
 	void applyMove(const Move& move);
+
+	/// <summary>
+	/// Finds the king of the given color. The coordinates of the king are set to the variables x and y.
+	/// </summary>
+	/// <param name="isWhite"></param>
+	/// <param name="x"></param>
+	/// <param name="y"></param>
+	/// <returns>The pointer to the king, or nullptr if the king was not found.</returns>
 	King* findKing(bool isWhite, int& x, int& y) const;
+
+	/// <summary>
+	/// Prints the board content to the console.
+	/// </summary>
 	void printBoard() const;
+
+	/// <summary>
+	/// Returns the piece at the given coordinates.
+	/// Does not check if the coordinates are valid!
+	/// </summary>
+	/// <param name="x"></param>
+	/// <param name="y"></param>
+	/// <returns>The piece at the given coordinates.</returns>
 	Piece* getPieceAt(int x, int y) const;
 
 	// Castling
