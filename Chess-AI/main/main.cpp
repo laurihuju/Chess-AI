fd83054b4c41b88f0c83e2087ddb3284dcec8c88
Tatu Vesalainen <156772244+tatuvesa@users.noreply.github.com>
2025-01-28 12:47:19 +0200

    Added colors to piece locations
    Added colors to prints for pieces.

--- Chess-AI/main/main.cpp
+++ Chess-AI/main/main.cpp
@@ -16,15 +16,19 @@ std::string indexToCoord(int x, int y) {
     return std::string(1, char('a' + x)) + std::string(1, char('8' - y));
 }
 
+// Helper function to set console text color
+void setConsoleColor(WORD color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
 int main() {
-	SetConsoleOutputCP(CP_UTF8);
-	GameState gameState;
-	gameState.printBoard();
+    SetConsoleOutputCP(CP_UTF8);
+    GameState gameState;
+    gameState.printBoard();
 
-	while (true) {
+    while (true) {
+        // Piece tests go after this line <----------------------------------------------
 
-		// Piece tests go after this line <----------------------------------------------
-		
         // Test for kings
         for (int i = 0; i < 8; ++i) {
             for (int j = 0; j < 8; ++j) {
@@ -32,7 +36,9 @@ int main() {
                 if (kingTest && kingTest->isWhite()) {
                     std::vector<Move> moves;
                     kingTest->possibleMoves(moves, i, j, gameState);
+                    setConsoleColor(FOREGROUND_RED | FOREGROUND_INTENSITY); // Set color for king
                     std::cout << "wK at: " << indexToCoord(i, j) << "; moves:";
+                    setConsoleColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // Reset color
                     if (moves.empty()) {
                         std::cout << " N/A";
                     }
@@ -53,10 +59,13 @@ int main() {
                 if (queenTest && queenTest->isWhite()) {
                     std::vector<Move> moves;
                     queenTest->possibleMoves(moves, i, j, gameState);
+                    setConsoleColor(FOREGROUND_BLUE | FOREGROUND_INTENSITY); // Set color for queen
                     std::cout << "wQ at: " << indexToCoord(i, j) << "; moves:";
+                    setConsoleColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // Reset color
                     if (moves.empty()) {
                         std::cout << " N/A";
-                    } else {
+                    }
+                    else {
                         for (const auto& move : moves) {
                             std::cout << " " << indexToCoord(move.x2(), move.y2());
                         }
@@ -74,10 +83,13 @@ int main() {
                 if (queenTest == 0 && bishopTest && bishopTest->isWhite()) {
                     std::vector<Move> moves;
                     bishopTest->possibleMoves(moves, i, j, gameState);
+                    setConsoleColor(FOREGROUND_GREEN | FOREGROUND_INTENSITY); // Set color for bishop
                     std::cout << "wB at: " << indexToCoord(i, j) << "; moves:";
+                    setConsoleColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // Reset color
                     if (moves.empty()) {
                         std::cout << " N/A";
-                    } else {
+                    }
+                    else {
                         for (const auto& move : moves) {
                             std::cout << " " << indexToCoord(move.x2(), move.y2());
                         }
@@ -95,10 +107,13 @@ int main() {
                 if (queenTest == 0 && rookTest && rookTest->isWhite()) {
                     std::vector<Move> moves;
                     rookTest->possibleMoves(moves, i, j, gameState);
+                    setConsoleColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_INTENSITY); // Set color for rook
                     std::cout << "wR at: " << indexToCoord(i, j) << "; moves:";
+                    setConsoleColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // Reset color
                     if (moves.empty()) {
                         std::cout << " N/A";
-                    } else {
+                    }
+                    else {
                         for (const auto& move : moves) {
                             std::cout << " " << indexToCoord(move.x2(), move.y2());
                         }
@@ -115,10 +130,13 @@ int main() {
                 if (knightTest && knightTest->isWhite()) {
                     std::vector<Move> moves;
                     knightTest->possibleMoves(moves, i, j, gameState);
+                    setConsoleColor(FOREGROUND_RED | FOREGROUND_BLUE | FOREGROUND_INTENSITY); // Set color for knight
                     std::cout << "wN at: " << indexToCoord(i, j) << "; moves:";
+                    setConsoleColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // Reset color
                     if (moves.empty()) {
                         std::cout << " N/A";
-                    } else {
+                    }
+                    else {
                         for (const auto& move : moves) {
                             std::cout << " " << indexToCoord(move.x2(), move.y2());
                         }
@@ -128,41 +146,41 @@ int main() {
             }
         }
 
-		// Test for pawns
-		for (int i = 0; i < 8; ++i) {
-			for (int j = 0; j < 8; ++j) {
-				Pawn* pawnTest = dynamic_cast<Pawn*>(gameState.getPieceAt(i, j));
-				if (pawnTest && pawnTest->isWhite()) {
+        // Test for pawns
+        for (int i = 0; i < 8; ++i) {
+            for (int j = 0; j < 8; ++j) {
+                Pawn* pawnTest = dynamic_cast<Pawn*>(gameState.getPieceAt(i, j));
+                if (pawnTest && pawnTest->isWhite()) {
                     std::vector<Move> moves;
-					pawnTest->possibleMoves(moves, i, j, gameState);
-					std::cout << "wP at: " << indexToCoord(i, j) << "; moves:";
+                    pawnTest->possibleMoves(moves, i, j, gameState);
+                    setConsoleColor(FOREGROUND_GREEN | FOREGROUND_BLUE | FOREGROUND_INTENSITY); // Set color for pawn
+                    std::cout << "wP at: " << indexToCoord(i, j) << "; moves:";
+                    setConsoleColor(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE); // Reset color
                     if (moves.empty()) {
                         std::cout << " N/A";
-                    } else {
+                    }
+                    else {
                         for (const auto& move : moves) {
                             std::cout << " " << indexToCoord(move.x2(), move.y2());
                         }
                     }
                     std::cout << "\n";
-				}
-			}
-		}
+                }
+            }
+        }
 
-		// Piece tests go before this line <----------------------------------------------
+        // Piece tests go before this line <----------------------------------------------
 
-		std::string input;
-		std::cin >> input;
+        std::string input;
+        std::cin >> input;
 
-		Move move(input);
-		gameState.applyMove(move);
-		gameState.printBoard();
-	}
+        Move move(input);
+        gameState.applyMove(move);
+        gameState.printBoard();
+    }
 
-    // Collect and print moves for all white queens
-    
-
-
-
-
-	return 0;
+    return 0;
 }
+
+
+
