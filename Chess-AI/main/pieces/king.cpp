c9e6d65c19fc16ce0fe75f797eddf8ac6ada46ac
laurihuju <lauri.huju@gmail.com>
2025-02-15 13:13:48 +0200

    Fix bug in castling move generation for black

--- Chess-AI/main/pieces/king.cpp
+++ Chess-AI/main/pieces/king.cpp
@@ -65,12 +65,12 @@ void King::possibleMoves(std::vector<Move>& moves, int x, int y, const GameState
 
 		// Left castling
 		if ((isWhite() ? gameState.lowerLeftCastlingPossible() : gameState.upperLeftCastlingPossible()) && gameState.getPieceAt(1, castlingRow) == 0 && gameState.getPieceAt(2, castlingRow) == 0 && gameState.getPieceAt(3, castlingRow) == 0 && !gameState.isThreatened(isWhite(), 2, castlingRow) && !gameState.isThreatened(isWhite(), 3, castlingRow)) {
-			moves.push_back(Move(4, 7, 2, 7));
+			moves.push_back(Move(4, castlingRow, 2, castlingRow));
 		}
 
 		// Right castling
-		if ((isWhite() ? gameState.lowerRightCastlingPossible() : gameState.upperRightCastlingPossible()) && gameState.getPieceAt(5, castlingRow) == 0 && gameState.getPieceAt(6, castlingRow) == 0 && !gameState.isThreatened(true, 5, castlingRow) && !gameState.isThreatened(isWhite(), 6, castlingRow)) {
-			moves.push_back(Move(4, 7, 6, 7));
+		if ((isWhite() ? gameState.lowerRightCastlingPossible() : gameState.upperRightCastlingPossible()) && gameState.getPieceAt(5, castlingRow) == 0 && gameState.getPieceAt(6, castlingRow) == 0 && !gameState.isThreatened(isWhite(), 5, castlingRow) && !gameState.isThreatened(isWhite(), 6, castlingRow)) {
+			moves.push_back(Move(4, castlingRow, 6, castlingRow));
 		}
 	}
 
